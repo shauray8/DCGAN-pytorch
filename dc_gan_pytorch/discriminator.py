9948b2bec15c62fbcf87c13f44393e6c34036b7b
Shauray <shauray9@gmail.com>
2021-01-24 00:18:38 +0530

    some changes in discriminator and model.py

--- dc_gan_pytorch/discriminator.py
+++ dc_gan_pytorch/discriminator.py
@@ -9,25 +9,25 @@ import numpy
 class Discriminator_block(nn.Module):
     def __init__(self, input_size, features):
         super(Discriminator_block, self).__init__()
-            self.conv1 = nn.Sequential(
-                        # 64*64
-                        
-                        nn.Conv2d(input_size, features, kernel_size=4, stride=2, padding=1),
-                        nn.LeakyReLU(.2),
-                        # 32*32
-                        
-                        self.batchnorm(features, features*2, 4, 2, 1),
-                        # 16*16
-                        
-                        self.batchnorm(featuresi*2, features*4, 4, 2, 1),
-                        # 8*8
-                        
-                        self.batchnorm(features*4, features*8, 4, 2, 1),
-                        # 4*4
+        self.conv1 = nn.Sequential(
+            # 64*64
+                
+            nn.Conv2d(input_size, features, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(.2),
+            # 32*32
+            
+            self.batchnorm(features, features*2, 4, 2, 1),
+            # 16*16
+            
+            self.batchnorm(featuresi*2, features*4, 4, 2, 1),
+            # 8*8
+            
+            self.batchnorm(features*4, features*8, 4, 2, 1),
+            # 4*4
 
-                        nn.Conv2d(features*8, 1, kernel_size=4, stride=2, padding=0),
-                        nn.Sigmoid(),
-                    )
+            nn.Conv2d(features*8, 1, kernel_size=4, stride=2, padding=0),
+            nn.Sigmoid(),
+        )
 
     # Layers with batch normalization 
     def batchnorm(self, input, out, kernel_size, stride, padding):
---
+++ dc_gan_pytorch/model.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+from discriminator import Discriminator_block
+from generator import Generator_block
+
+
+print('Done')
